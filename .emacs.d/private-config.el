4a8fb3015e9d718210ebf6b09dfd5561f8805318
Rajath Ramakrishna <r.rajath@protonmail.com>
2021-09-06 22:35:14 -0700

    remap <tab> to `evil-toggle-fold`
    This solves the issue of drawers not expanding

--- .emacs.d/private-config.el
+++ .emacs.d/private-config.el
@@ -833,34 +833,10 @@ If on a:
  :keymaps 'org-mode-map
  "RET" '+org/dwim-at-point)
 
-(defun +org-cycle-only-current-subtree-h (&optional arg)
-  "Toggle the local fold at the point, and no deeper.
-       `org-cycle's standard behavior is to cycle between three levels: collapsed,
-       subtree and whole document. This is slow, especially in larger org buffer. Most
-       of the time I just want to peek into the current subtree -- at most, expand
-       *only* the current subtree.
-
-       All my (performant) foldings needs are met between this and `org-show-subtree'
-       (on zO for evil users), and `org-cycle' on shift-TAB if I need it."
-  (interactive "P")
-  (unless (eq this-command 'org-shifttab)
-    (save-excursion
-      (org-beginning-of-line)
-      (let (invisible-p)
-        (when (and (org-at-heading-p)
-                   (or org-cycle-open-archived-trees
-                       (not (member org-archive-tag (org-get-tags))))
-                   (or (not arg)
-                       (setq invisible-p (outline-invisible-p (line-end-position)))))
-          (unless invisible-p
-            (setq org-cycle-subtree-status 'subtree))
-          (org-cycle-internal-local)
-          t)))))
-
 (general-define-key
  :states 'normal
  :keymaps 'org-mode-map
- "<tab>" '+org-cycle-only-current-subtree-h)
+ "<tab>" 'evil-toggle-fold)
 
 (defun +org--insert-item (direction)
   (let ((context (org-element-lineage
