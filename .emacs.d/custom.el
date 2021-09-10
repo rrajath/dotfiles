4b9758a98840284464b61f0cf036dcb9dbe92d4e
Rajath Ramakrishna <rajath@amazon.com>
2021-09-10 11:02:30 -0700

    new package added: beacon
    To highlight current line so as to not lose your cursor

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,10 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
 	 '("e29a6c66d4c383dbda21f48effe83a1c2a1058a17ac506d60889aba36685ed94" default))
- '(excorporate-configuration nil)
  '(ivy-rich-mode t)
  '(package-selected-packages
-	 '(no-littering embark orderless ws-butler evil-nerd-commenter smart-mode-line-atom-one-dark-theme smart-mode-line doct use-package diredfl dired-fl all-the-icons-dired dired-rainbow dired-collapse dired-ranger dired-single expand-region evil-org perspective super-save marginalia vertico vterm-toggle vterm lookup visual-fill-column org-bullets evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes helpful which-key rainbow-delimiters ivy-rich counsel doom-modeline ivy))
+	 '(peep-dired ranger esup beacon ox-gfm no-littering embark orderless ws-butler evil-nerd-commenter smart-mode-line-atom-one-dark-theme smart-mode-line doct use-package diredfl dired-fl all-the-icons-dired dired-rainbow dired-collapse dired-ranger dired-single expand-region evil-org perspective super-save marginalia vertico vterm-toggle vterm lookup visual-fill-column org-bullets evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes helpful which-key rainbow-delimiters ivy-rich counsel doom-modeline ivy))
  '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
