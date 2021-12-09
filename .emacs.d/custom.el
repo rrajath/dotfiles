ae409a8b72ffd514a32e1d43d3655884f366d279
Rajath Ramakrishna <rajath@amazon.com>
2021-12-09 11:17:55 -0800

    chore: change keybindings for beginning and end of line
    The latest changes in evil-org packge broke it and the keybindings had
    to be remapped

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,7 +7,7 @@
 	 '("e29a6c66d4c383dbda21f48effe83a1c2a1058a17ac506d60889aba36685ed94" default))
  '(ivy-rich-mode t)
  '(package-selected-packages
-	 '(ox-hugo org-make-toc no-littering embark orderless ws-butler evil-nerd-commenter smart-mode-line-atom-one-dark-theme smart-mode-line doct use-package diredfl dired-fl all-the-icons-dired dired-rainbow dired-collapse dired-ranger dired-single expand-region evil-org perspective super-save marginalia vertico vterm-toggle vterm lookup visual-fill-column org-bullets evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes helpful which-key rainbow-delimiters ivy-rich counsel doom-modeline ivy))
+	 '(workgroups2 centaur-tabs no-littering embark orderless ws-butler evil-nerd-commenter smart-mode-line-atom-one-dark-theme smart-mode-line doct use-package diredfl dired-fl all-the-icons-dired dired-rainbow dired-collapse dired-ranger dired-single expand-region evil-org perspective super-save marginalia vertico vterm-toggle vterm lookup visual-fill-column org-bullets evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes helpful which-key rainbow-delimiters ivy-rich counsel doom-modeline ivy))
  '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
