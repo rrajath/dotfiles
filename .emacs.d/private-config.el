1ee495c9c61a1b42656d3fa53723b3f56d3914bc
Rajath Ramakrishna <r.rajath@protonmail.com>
2023-01-14 20:30:17 -0800

    remove evil
    Evil is now replaced with Meow

--- .emacs.d/private-config.el
+++ .emacs.d/private-config.el
@@ -325,9 +325,6 @@ soon as Emacs loads."
 ;; Revert buffers when the underlying file has changed
 (global-auto-revert-mode 1)
 
-(use-package evil-nerd-commenter
-  :after evil)
-
 (defun rr/comment-and-nextline ()
   "Comment the current line and move the point to the next line"
   (interactive)
@@ -451,32 +448,6 @@ soon as Emacs loads."
   :config
   (which-key-mode)
   (setq which-key-idle-delay 0.3))
-
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-model 'normal))
-
-(evil-mode 1)
-
-;; Evil Collection for predictable Vim keybindings in a lot of modes
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
 
 (general-define-key
  :states 'normal
