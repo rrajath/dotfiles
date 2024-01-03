9a5d55145fb848177ddc4c0dc474a7609dbc66ee
Rajath Ramakrishna <rajath.ramakrishna@getcruise.com>
2024-01-02 17:47:03 -0800

    do not tangle centaur tabs
    Not using it anymore

--- .emacs.d/private-config.el
+++ .emacs.d/private-config.el
@@ -874,65 +874,6 @@ folder, otherwise delete a word"
 (use-package crux
   :defer t)
 
-(use-package centaur-tabs
-  :demand
-  :config
-  (centaur-tabs-mode t)
-  (setq
-   centaur-tabs-cycle-scope 'tabs
-   centaur-tabs-style "box"
-   centaur-tabs-set-icons t
-   centaur-tabs-gray-out-icons 'buffer
-   centaur-tabs-height 32
-   centaur-tabs-set-bar 'under
-   centaur-tabs-set-modified-marker t
-   x-underline-at-descent-line t
-   uniquify-separator "/"
-   uniquify-buffer-name-style 'forward)
-  (defun centaur-tabs-buffer-groups ()
-    "`centaur-tabs-buffer-groups' control buffers' group rules.
-
-Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
-All buffer name start with * will group to \"Emacs\".
-Other buffer group by `centaur-tabs-get-group-name' with project name."
-    (list
-     (cond
-      ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
-      ;; "Remote")
-      ((or (string-equal "*" (substring (buffer-name) 0 1))
-           (memq major-mode '(magit-process-mode
-                              magit-status-mode
-                              magit-diff-mode
-                              magit-log-mode
-                              magit-file-mode
-                              magit-blob-mode
-                              magit-blame-mode
-                              )))
-       "Emacs")
-      ((derived-mode-p 'prog-mode)
-       "Editing")
-      ((derived-mode-p 'dired-mode)
-       "Dired")
-      ((memq major-mode '(helpful-mode
-                          help-mode))
-       "Help")
-      ((memq major-mode '(org-mode
-                          org-agenda-clockreport-mode
-                          org-src-mode
-                          org-agenda-mode
-                          org-beamer-mode
-                          org-indent-mode
-                          org-bullets-mode
-                          org-cdlatex-mode
-                          org-agenda-log-mode
-                          diary-mode))
-       "OrgMode")
-      (t
-       (centaur-tabs-get-group-name (current-buffer))))))
-  :bind
-  ("C-S-<tab>" . centaur-tabs-backward)
-  ("C-<tab>" . centaur-tabs-forward))
-
 (use-package free-keys
   :defer t)
 
