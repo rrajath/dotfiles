98bb006ed2a49312d00a4e7d102e1c760544bd29
Rajath Ramakrishna <r.rajath@protonmail.com>
2024-08-10 15:36:35 -0700

    Set up better tree-sitter config for typescript
    The old config is still kept around and untangled for now.

--- .emacs.d/private-config.el
+++ .emacs.d/private-config.el
@@ -972,49 +972,12 @@ folder, otherwise delete a word"
 (use-package eat
   :defer t)
 
-(use-package apheleia
-  :ensure t
-  :hook ((typescriptreact-mode . aphelia-mode))
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
   :config
-  (apheleia-global-mode +1))
-
-(use-package tree-sitter
-  :ensure t
-  :config
-  ;; activate tree-sitter on any buffer containing code for which it has a parser available
-  (global-tree-sitter-mode)
-  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
-  ;; by switching on and off
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
-
-(use-package tree-sitter-langs
-  :ensure t
-  :after tree-sitter)
-
-(use-package typescript-mode
-  :after tree-sitter
-  :config
-  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
-  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
-  (define-derived-mode typescriptreact-mode typescript-mode
-    "TypeScript TSX")
-
-  ;; use our derived mode for tsx files
-  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
-  ;; by default, typescript-mode is mapped to the treesitter typescript parser
-  ;; use our derived mode to map both .tsx AND .ts - typescriptreact-mode - treesitter tsx
-  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
-
-(use-package tsi
-  :after tree-sitter
-  :straight (tsi :host github :repo "orzechowskid/tsi.el")
-  ;; define autoload definitions which when actually invoked will cause package to be loaded
-  :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
-  :init
-  (add-hook 'typescript-mode-hook (lambda () (tsi-typescript-mode 1)))
-  (add-hook 'json-mode-hook (lambda () (tsi-json-mode 1)))
-  (add-hook 'css-mode-hook (lambda () (tsi-css-mode 1)))
-  (add-hook 'scss-mode-hook (lambda () (tsi-scss-mode 1))))
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
 
 (straight-use-package
  '(flymake-popon
@@ -1024,13 +987,25 @@ folder, otherwise delete a word"
 (use-package jsonrpc
   :defer t)
 
+(use-package typescript-ts-mode
+	:mode "\\.ts\\'"
+	:hook (typescript-ts-mode . eglot-ensure))
+
 (use-package eglot
   :hook (
-         (typescriptreact-mode . eglot-ensure)
+         ((typescriptreact-mode typescript-ts-mode) . eglot-ensure)
+				 (go-ts-mode . eglot-ensure)
          (typescriptreact-mode . flymake-popon-mode)
          )
   :config
-  (setq eglot-confirm-server-initiated-edits nil))
+  (setq eglot-confirm-server-initiated-edits nil)
+	(setq eglot-ignored-server-capabilities nil)
+	(add-to-list 'eglot-server-programs
+							 '((typescript-ts-mode typescript-mode) . ("typescript-language-server" "--stdio"))))
+
+(use-package prettier)
+(setq prettier-mode-sync-config-flag nil)
+(add-hook 'after-init-hook #'global-prettier-mode)
 
 (use-package restclient
   :defer t)
