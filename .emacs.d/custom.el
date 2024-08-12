074dcb900d13ff3a47bde1255bac01fcc11022ab
Rajath Ramakrishna <r.rajath@protonmail.com>
2024-08-12 16:14:26 -0700

    update custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,10 +4,26 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("2a7d6f6a46b1b8504b269029a7375597f208a5cdfa1ea125b09255a592bb326e" "74367676a7b0562975704f8e576d5e103451527b36c9226a013cd8f3ae2140f5" "72ca2b0770981be70839fc2b738fc81f6268477653a6d89df67f0f94eb64e1e7" "e29a6c66d4c383dbda21f48effe83a1c2a1058a17ac506d60889aba36685ed94" default))
+	 '("e29a6c66d4c383dbda21f48effe83a1c2a1058a17ac506d60889aba36685ed94"
+		 default))
  '(ivy-rich-mode t)
  '(package-selected-packages
-	 '(no-littering embark orderless ws-butler evil-nerd-commenter smart-mode-line-atom-one-dark-theme smart-mode-line doct use-package diredfl dired-fl all-the-icons-dired dired-rainbow dired-collapse dired-ranger dired-single expand-region evil-org perspective super-save marginalia vertico vterm-toggle vterm lookup visual-fill-column org-bullets evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes helpful which-key rainbow-delimiters ivy-rich counsel doom-modeline ivy))
+	 '(no-littering embark orderless ws-butler evil-nerd-commenter
+									smart-mode-line-atom-one-dark-theme smart-mode-line
+									doct use-package diredfl dired-fl
+									all-the-icons-dired dired-rainbow dired-collapse
+									dired-ranger dired-single expand-region evil-org
+									perspective super-save marginalia vertico
+									vterm-toggle vterm lookup visual-fill-column
+									org-bullets evil-magit magit counsel-projectile
+									projectile hydra evil-collection general doom-themes
+									helpful which-key rainbow-delimiters ivy-rich
+									counsel doom-modeline ivy))
+ '(safe-local-variable-values
+	 '((org-roam-db-location
+			. "/Users/rajath.ramakrishna/org-mode/roam/work/org-roam.db")
+		 (org-roam-directory
+			. "/Users/rajath.ramakrishna/org-mode/roam/work")))
  '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
