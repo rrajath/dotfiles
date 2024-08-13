4d95704b9f5239839b23d21c3c4188b4b89c9f88
Rajath Ramakrishna <rajath.ramakrishna@getcruise.com>
2024-08-12 17:12:42 -0700

    Move existing .emacs.d directory to deprecated/

---
+++ deprecated/.emacs.d/custom.el
@@ -0,0 +1,33 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 '("e29a6c66d4c383dbda21f48effe83a1c2a1058a17ac506d60889aba36685ed94"
+		 default))
+ '(ivy-rich-mode t)
+ '(package-selected-packages
+	 '(no-littering embark orderless ws-butler evil-nerd-commenter
+									smart-mode-line-atom-one-dark-theme smart-mode-line
+									doct use-package diredfl dired-fl
+									all-the-icons-dired dired-rainbow dired-collapse
+									dired-ranger dired-single expand-region evil-org
+									perspective super-save marginalia vertico
+									vterm-toggle vterm lookup visual-fill-column
+									org-bullets evil-magit magit counsel-projectile
+									projectile hydra evil-collection general doom-themes
+									helpful which-key rainbow-delimiters ivy-rich
+									counsel doom-modeline ivy))
+ '(safe-local-variable-values
+	 '((org-roam-db-location
+			. "/Users/rajath.ramakrishna/org-mode/roam/work/org-roam.db")
+		 (org-roam-directory
+			. "/Users/rajath.ramakrishna/org-mode/roam/work")))
+ '(which-key-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
