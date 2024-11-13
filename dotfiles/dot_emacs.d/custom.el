f67b0df36c8fe3ae0c54b465283805c8ded8ba14
rrajath <r.rajath@protonmail.com>
2024-11-13 09:35:40 -0800

    Update .emacs.d/PostInitConfig.org Add dotfiles/.emacs.d/PostInitConfig.org Add dotfiles/.emacs.d/PreInitConfig.org Add dotfiles/.emacs.d/README.md Add dotfiles/.emacs.d/custom.el Add dotfiles/.emacs.d/.LICENSE Add dotfiles/.emacs.d/.gitignore Add dotfiles/.emacs.d/.images/emacs-startup.png Add dotfiles/.emacs.d/early-init.el Add dotfiles/.emacs.d/init.el Add dotfiles/.emacs.d/org-persist/3c/767ce2-f496-44af-ba20-650f32b38f33 Add dotfiles/.emacs.d/org-persist/68/9da009-07a1-4941-b837-079ecb9c2a12 Add dotfiles/.emacs.d/org-persist/index.eld Add dotfiles/.emacs.d/post-init.el Add dotfiles/.emacs.d/autosave/.keep Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-awk.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-bash.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-bibtex.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-blueprint.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-c-sharp.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-c.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-clojure.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-cmake.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-commonlisp.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-cpp.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-css.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-dart.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-dockerfile.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-elixir.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-glsl.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-go.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-gomod.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-heex.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-html.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-janet.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-java.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-javascript.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-json.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-julia.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-kotlin.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-lua.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-magik.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-make.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-nix.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-nu.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-org.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-perl.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-proto.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-python.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-r.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-ruby.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-rust.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-scala.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-sql.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-surface.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-toml.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-tsx.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-typescript.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-typst.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-verilog.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-vhdl.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-vue.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-wast.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-wat.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-wgsl.dylib Add dotfiles/.emacs.d/tree-sitter/libtree-sitter-yaml.dylib

---
+++ dotfiles/dot_emacs.d/custom.el
@@ -0,0 +1,25 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons-completion avy beacon corfu crux diff-hl dired dired-collapse
+                              dired-ranger dired-single diredfl doom-modeline
+                              doom-themes eat embark-consult embrace free-keys
+                              helpful highlight-symbol hungry-delete kind-icon
+                              magit marginalia meow no-littering orderless
+                              org-appear org-bullets org-make-toc org-roam-ui
+                              org-super-agenda ox-gfm ox-hugo perspective popper
+                              prettier rainbow-delimiters super-save
+                              treesit-auto vertico visual-fill-column))
+ '(package-vc-selected-packages
+   '((dired-single :url "https://codeberg.org/amano.kenji/dired-single")
+     (org-roam-ui :url "https://github.com/org-roam/org-roam-ui" :branch "main"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
