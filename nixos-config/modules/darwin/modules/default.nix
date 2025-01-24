e23116a25aad6a745a16cd345c0564d6f2ddd0dc
rrajath <r.rajath@protonmail.com>
2025-01-23 19:34:03 -0800

    nix: ignore generating aerospace config for now

--- nixos-config/modules/darwin/modules/default.nix
+++ nixos-config/modules/darwin/modules/default.nix
@@ -4,7 +4,7 @@
   home-manager.users.${user} = {
     home.file = {
       ".config/alacritty".source = ../../../../alacritty;
-      ".config/aerospace/aerospace.toml".source = ../../../../aerospace/aerospace.toml;
+#      ".config/aerospace/aerospace.toml".source = ../../../../aerospace/aerospace.toml;
       ".config/ghostty/config".source = ../../../../ghostty/ghostty.toml;
       ".wezterm.lua".source = ../../../../wezterm/wezterm.lua;
     };
