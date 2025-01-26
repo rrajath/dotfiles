e9cc92408a9bc5e0b494e1d0c1fa1f51338f7da8
rrajath <r.rajath@protonmail.com>
2025-01-26 13:43:01 -0800

    nix: manage karabiner using nix

--- nix/modules/darwin/programs/default.nix
+++ nix/modules/darwin/programs/default.nix
@@ -6,6 +6,7 @@
       ".config/alacritty".source = ../../../../alacritty;
       # ".config/aerospace/aerospace.toml".source = ../../../../aerospace/aerospace.toml;
       ".wezterm.lua".source = ../../../../wezterm/wezterm.lua;
+      ".config/karabiner".source = ../../../../karabiner;
     };
   };
 }
