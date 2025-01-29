de92594e26b64740daf55ffee2cd9f1227ba8a72
rrajath <r.rajath@protonmail.com>
2025-01-28 19:20:31 -0800

    jj: add config for git private commits
    Any commit that has a subject line starting with either "wip:" or "private:"
    will not be pushed. Any of their descendant commits will not be pushed either.

--- nix/modules/darwin/programs/jujutsu.nix
+++ nix/modules/darwin/programs/jujutsu.nix
@@ -10,6 +10,9 @@
         editor = "hx";
         default-command = "log";
       };
+      git = {
+        private-commits = "description(glob:'wip:*') | description(glob:'private:*')";
+      };
       template-aliases = {
         "format_short_id(id)" = "id.shortest()";
         "format_timestamp(timestamp)" = "timestamp.ago()";
