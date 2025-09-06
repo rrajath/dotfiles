ccb314e8bd9f00a5fc596201b60d4f7d8a53074f
rrajath <r.rajath@protonmail.com>
2025-09-06 01:07:59 -0700

    nix: add some nix options to shared/darwin.nix

--- nix-config/shared/darwin.nix
+++ nix-config/shared/darwin.nix
@@ -4,8 +4,26 @@
     ../modules/macos-settings.nix
   ];
   
-  # Enable experimental features for flakes
-  nix.settings.experimental-features = "nix-command flakes";
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      trusted-users = [ "@admin" "${user}" ];
+      substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+    };
+
+    gc = {
+      automatic = true;
+      interval = { Weekday = 0; Hour = 2; Minute = 0; };
+      options = "--delete-older-than 30d";
+    };
+
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  services.emacs.enable = true;
   
   # Set Nushell as the default shell
   environment.shells = [ pkgs.nushell ];
