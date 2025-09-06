240be20b0f60c9be45c211d76158352f690b03ae
rrajath <r.rajath@protonmail.com>
2025-09-06 01:07:59 -0700

    jj: add tug alias to jujutsu

--- nix-config/modules/jujutsu.nix
+++ nix-config/modules/jujutsu.nix
@@ -19,6 +19,9 @@
       git = {
         private-commits = "description(glob:'wip:*') | description(glob:'private:*')";
       };
+      aliases = {
+        tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
+      };
       template-aliases = {
         "format_short_id(id)" = "id.shortest()";
         "format_timestamp(timestamp)" = "timestamp.ago()";
