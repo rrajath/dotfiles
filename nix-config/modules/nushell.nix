f7954ca19ab28ad9d9f2a9e12f8d428419451b4c
rrajath <r.rajath@protonmail.com>
2025-09-13 17:02:44 -0700

    nushell: tweak config with some niceties

--- nix-config/modules/nushell.nix
+++ nix-config/modules/nushell.nix
@@ -5,6 +5,12 @@
     $env.config = {
       show_banner: false
       buffer_editor: "hx"
+      rm: {
+        always_trash: true
+      }
+      completions: {
+        algorithm: "fuzzy"
+      }
     }
     '';
     # Environment configuration
@@ -13,19 +19,24 @@
     $env.PATH = ($env.PATH | split row (char esep) | 
 	    prepend [
 	      "/etc/profiles/per-user/rrajath/bin"
-      	"/nix/var/nix/profiles/default/bin"
-      	"/Users/rrajath/.nix-profile/bin"
-        "/usr/local/bin"
-        "~/Library/Python/3.9/bin"
-        "/opt/homebrew/bin"
-        "/Users/rrajath/.local/bin"
-        "/Users/rrajath/.local"
+          "/nix/var/nix/profiles/default/bin"
+          "/Users/rrajath/.nix-profile/bin"
+          "/usr/local/bin"
+          "~/Library/Python/3.9/bin"
+          "/opt/homebrew/bin"
+          "/Users/rrajath/.local/bin"
+          "/Users/rrajath/.local"
     ])
     $env.CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense' # optional
     '';
     extraConfig = ''
       # Additional configuration that runs after the main config
       alias ll = ls -l
+      alias drp = sudo darwin-rebuild switch --flake ~/dotfiles/nix-config#personal
+      alias drw = sudo darwin-rebuild switch --flake ~/dotfiles/nix-config#work
+
+      # other config
+      # rm.always_trash = true;
     '';
   };
 }
